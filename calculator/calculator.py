d0ad723584a3f5e152d9c992f4c56003cde5dad9
Arthur-Fernandes-Barros <arthurfernandesbarros@gmail.com>
2025-06-24 14:35:10 -0300

    Calculator using functions and using a simple output format

---
+++ calculator/calculator.py
@@ -0,0 +1,31 @@
+# Write code below 💖
+
+def add(a,b):
+    
+    answer= a + b
+    return answer
+
+def multiply(a,b):
+    
+    answer= a * b
+    return answer
+
+def divide(a,b):
+    
+    answer= a / b
+    return answer
+
+def exp(a,b):
+    
+    answer= a ** b
+    return answer
+
+def subtract(a,b):
+        answer= a - b
+        return answer
+    
+print(add(2,3))
+print(multiply(2,3))       
+print(divide(6,3))
+print(exp(2,3))        
+print(subtract(5,3))
