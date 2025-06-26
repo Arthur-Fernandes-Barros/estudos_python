b7f0579cab6d6b54112557eed7f4eb837603b5b7
Arthur-Fernandes-Barros <arthurfernandesbarros@gmail.com>
2025-06-26 12:18:39 -0300

    A birthday count script in Python

---
+++ bdaymessage/main.py
@@ -0,0 +1,14 @@
+import datetime 
+import bday_message
+
+today = datetime.date.today()
+
+next_birthday = datetime.date(2025,6,26)
+
+days_away =   next_birthday - today
+
+if today == next_birthday:
+    print(bday_message.radom_message)
+else:
+    print(f"My next birthday is {days_away} days away!")
+
